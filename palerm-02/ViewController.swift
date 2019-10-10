9b9c599caf9e9f2def9a83671841d52a38239c53
shuufei <digital.shuufei@gmail.com>
2019-10-10 21:47:17 +0900

    Initial Commit

---
+++ palerm-02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  palerm-02
+//
+//  Created by shuuhei-hanashiro on 2019/10/10.
+//  Copyright © 2019 shuuhei-hanashiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
